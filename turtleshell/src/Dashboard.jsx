08ad8c7f85a4930e66334c59b868991fc10a08c5
keinberger <philipp.keinberger@gmail.com>
2023-03-05 04:31:43 -0700

    implemented frontend svg generation

--- turtleshell/src/Dashboard.jsx
+++ turtleshell/src/Dashboard.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import Sidebar from "./dashboard/Sidebar";
-import { Outlet } from "react-router-dom";
-import Header from "./dashboard/Header";
-import { Navigate } from "react-router-dom";
-import { useAccount } from "wagmi";
+import React from "react"
+import Sidebar from "./dashboard/Sidebar"
+import { Outlet } from "react-router-dom"
+import Header from "./dashboard/Header"
+import { Navigate } from "react-router-dom"
+import { useAccount } from "wagmi"
 
 const Dashboard = () => {
-  
+	return (
+		<div className="flex">
+			<Sidebar />
+			<div className="flex-1 m-5">
+				<Header />
+				<Outlet />
+			</div>
+		</div>
+	)
+}
 
-  return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 m-5">
-        <Header />
-        <Outlet />
-      </div>
-    </div>
-  );
-};
-
-export default Dashboard;
+export default Dashboard
--- turtleshell/src/pages/MintProof.jsx
+++ turtleshell/src/pages/MintProof.jsx
@@ -1,32 +1,39 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getImgUrlIpfs } from '../utils.js';
-import BadgeSvg from '../components/BadgeSvg.jsx';
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { getImgUrlIpfs } from "../utils.js"
+import BadgeSvg from "../components/BadgeSvg.jsx"
 
 const MintProof = () => {
-  const { id, hash }  = useParams();
-  const [ipfsUrl, setIpfsUrl] = useState('');
+    const { id, hash, auditDetailsParsed } = useParams()
+    const [ipfsUrl, setIpfsUrl] = useState("")
 
-  useEffect(() => {
-    if (id && hash) {
-      async function getImgUrl(ipfsHash) {
-        const url = await getImgUrlIpfs(ipfsHash);
-        setIpfsUrl(url);
-      }
-      getImgUrl(hash);
-      console.log(id, hash)
-    }
-  }, [id, hash])
+    const auditDetails = JSON.parse(auditDetailsParsed)
 
-  return (
-    <div className='text-white'>
-      <h1>Mint Proof</h1>
-      {
-        ipfsUrl ? <img src={ipfsUrl} /> : <p>Loading...</p>
-      }
-      <BadgeSvg />
-    </div>
-  );
+    useEffect(() => {
+        if (id && hash) {
+            async function getImgUrl(ipfsHash) {
+                const url = await getImgUrlIpfs(ipfsHash)
+                setIpfsUrl(url)
+            }
+            getImgUrl(hash)
+            console.log(id, hash)
+        }
+    }, [id, hash])
+
+    return (
+        <div className="text-white">
+            <h1>Mint Proof</h1>
+            {ipfsUrl ? <img src={ipfsUrl} /> : <p>Loading...</p>}
+            <BadgeSvg
+                TOKEN_ID={auditDetails.tokenId}
+                NETWORK_NAME={"GOERLI"}
+                RISK_LEVEL={auditDetails.riskLevel}
+                TIMESTAMP={auditDetails.timestamp}
+                CONTRACT_ADDRESS={auditDetails.contractAddr}
+                TYPES_VULNERABILITIES={auditDetails.types}
+            />
+        </div>
+    )
 }
 
-export default MintProof;
+export default MintProof
