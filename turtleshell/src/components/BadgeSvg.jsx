99613012c1de6dfabc106673ce437b2231829464
judygab <ivditigabeskiria@gmail.com>
2023-03-05 07:33:22 -0500

    update svg size

--- turtleshell/src/components/BadgeSvg.jsx
+++ turtleshell/src/components/BadgeSvg.jsx
@@ -10,6 +10,7 @@ export default function BadgeSvg({
         <svg
             fill="none"
             viewBox="0 0 549 507"
+            className="w-30 h-30"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
         >
