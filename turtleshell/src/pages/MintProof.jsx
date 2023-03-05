273acb42d96d6aba427468b84c010d8468149def
judygab <ivditigabeskiria@gmail.com>
2023-03-05 06:35:42 -0500

    added signature hook

--- turtleshell/src/pages/MintProof.jsx
+++ turtleshell/src/pages/MintProof.jsx
@@ -2,21 +2,26 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getImgUrlIpfs } from '../utils.js';
 import BadgeSvg from '../components/BadgeSvg.jsx';
+import Button from '../components/Common/Button.jsx';
+import useSignature from '../hooks/useSignature.js';
+import { useReward } from 'react-rewards';
 
 const MintProof = () => {
   const { id, hash }  = useParams();
   const [ipfsUrl, setIpfsUrl] = useState('');
+  const { data, error, loading, createSignature } = useSignature();
+  const [mintSuccess, setMintSuccess] = useState(false);
+  const { reward, isAnimating } = useReward('rewardId', 'confetti');
 
   useEffect(() => {
-    if (id && hash) {
-      async function getImgUrl(ipfsHash) {
-        const url = await getImgUrlIpfs(ipfsHash);
-        setIpfsUrl(url);
-      }
-      getImgUrl(hash);
-      console.log(id, hash)
+    if (mintSuccess) {
+      reward();
     }
-  }, [id, hash])
+  }, [mintSuccess]);
+
+  const handleCreateSignature = async () => {
+    await createSignature(hash, id);
+  };
 
   return (
     <div className='text-white'>
@@ -25,6 +30,16 @@ const MintProof = () => {
         ipfsUrl ? <img src={ipfsUrl} /> : <p>Loading...</p>
       }
       <BadgeSvg />
+      <div className='flex items-center justify-center'>
+        {!mintSuccess && <Button onClick={handleCreateSignature}>Mint</Button>}
+      </div>
+      {mintSuccess && 
+      <div className="flex items-center justify-center text-white">
+        <h2 className='text-xl relative'>
+          Minted Successfully
+          <span id="rewardId" style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)'}}/>
+        </h2>
+      </div>}
     </div>
   );
 }
